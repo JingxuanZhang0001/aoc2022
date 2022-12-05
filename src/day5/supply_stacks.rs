7d5a54f138e04d129005250d845181fd0676635b
Jingxuan Zhang <Jingxuan Zhang>
2022-12-04 23:24:27 -0800

    day5

---
+++ src/day5/supply_stacks.rs
@@ -0,0 +1,65 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use regex::Regex;
+use lazy_static::lazy_static;
+
+pub fn solution() {
+    let file = File::open("./src/day5/input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut cargo: Vec<Vec<char>> = vec![];
+
+    for line in reader.lines() {
+        let instruction = line.unwrap();
+        if instruction == "" {
+            for v in &mut cargo {
+                v.reverse();
+                // print!("{}", v.get(v.len() - 1).unwrap_or(&' '));
+            }
+            // println!();
+            continue;
+        }
+        if instruction.chars().next().unwrap() == '[' {
+            let n = (instruction.len() + 1) / 4;
+            while cargo.len() < n {
+                cargo.push(vec![]);
+            }
+            for (idx, ch) in instruction.chars().enumerate() {
+                if idx % 4 == 1 && ch.is_alphabetic() {
+                    cargo[idx / 4].push(ch);
+                }
+            }
+            continue;
+        }
+
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+        }
+
+        let capture = RE.captures(&instruction);
+
+        if capture.is_none() {
+            continue;
+        }
+        
+        let (num, from, to) = capture.map(|cap| {
+            (cap.get(1).unwrap().as_str().parse::<usize>().unwrap(),
+            cap.get(2).unwrap().as_str().parse::<usize>().unwrap(),
+            cap.get(3).unwrap().as_str().parse::<usize>().unwrap())
+        }).unwrap();
+        // part 1
+        // for _ in 0..num {
+        //     let top = cargo[from - 1].pop().unwrap();
+        //     cargo[to - 1].push(top);
+        // }
+        
+        // part 2
+        let cut_start = cargo[from - 1].len() - num;
+        let moving: Vec<char> = cargo[from - 1].drain(cut_start..).collect();
+        cargo[to - 1].extend_from_slice(&moving);
+    }
+    for v in cargo {
+        print!("{}", v.get(v.len() - 1).unwrap_or(&' '));
+    }
+    println!();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // pub mod day1;
-pub mod day4;
+pub mod day5;
 
 fn main() {
     // day1::calorie_counting::solution();
     // day2::rock_paper_scissors::solution();
     // day3::rucksack_reorganization::solution();
-    day4::camp_cleanup::solution();
+    // day4::camp_cleanup::solution();
+    day5::supply_stacks::solution();
 }
